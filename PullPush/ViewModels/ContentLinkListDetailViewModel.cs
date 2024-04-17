8a2a7f137e0f6b430d98f88755e10a66b6a04b79
SazanamiInko <s.suzuki1614@gmail.com>
2024-04-17 10:20:47 +0900

    紐づけルール詳細編集画面の構成

--- PullPush/ViewModels/ContentLinkListDetailViewModel.cs
+++ PullPush/ViewModels/ContentLinkListDetailViewModel.cs
@@ -1,4 +1,9 @@
-﻿using Common;
+﻿using AutoMapper;
+using Common;
+using FLayer.APIs;
+using FLayer.Responses;
+using Interfaces.DataModel;
+using Interfaces.Response;
 
 namespace PullPush.ViewModels;
 
@@ -8,14 +13,69 @@ public partial class ContentLinkListDetailViewModel : BaseViewModel
 	[ObservableProperty]
     SubContebtViewViewDataModel item;
 
-    #region MyRegion
+
+    /// <summary>
+    /// 科目
+    /// </summary>
+    [ObservableProperty]
+    ObservableCollection<SubjectViewDataModel> subjects;
+
+    /// <summary>
+    /// 選択されてる科目
+    /// </summary>
+    [ObservableProperty]
+    SubjectViewDataModel selctedSubhect;
+
+    #region コンストラクタ
     /// <summary>
     /// コンストラクタ
     /// </summary>
     /// <param name="logging"></param>
     public ContentLinkListDetailViewModel(LoggingService logging) : base(logging)
     {
+        this.InitializeVM();
+        API.InitializeAPI(logging);
     }
     #endregion
 
+
+    #region メソッド
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public void InitializeVM()
+    {
+
+        var config = new MapperConfiguration(cfg => { cfg.CreateMap<ISubject, SubjectViewDataModel>(); });
+        Mapper map = new Mapper(config);
+
+        Logging.WriteLog(() => {
+            IResponse response = API.GetSubjextItems();
+
+            if (!response.Success)
+            {
+                return;
+            }
+
+            if (response is SubjectResponse)
+            {
+                SubjectResponse subres = response as SubjectResponse;
+
+                this.Subjects = new ObservableCollection<SubjectViewDataModel>();
+                subres.Items.ForEach(record =>
+                {
+                    SubjectViewDataModel item = map.Map<SubjectViewDataModel>(record); ;
+                    this.Subjects.Add(item);
+                });
+
+                if (subres.Items.Count > 0)
+                {
+                    this.SelctedSubhect = this.Subjects[0];
+                }
+            }
+        });
+
+    }
+    #endregion
 }
